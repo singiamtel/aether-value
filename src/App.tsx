3c5ad56d232b0316dbed026ce73cb93d1177dcc4
singiamtel <sergiogarciacotos@gmail.com>
2021-06-23 21:54:15 +0200

    Router redirects to login if not logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { BrowserRouter, Redirect, Switch, Route } from "react-router-dom";
+import {  Redirect, Switch, Route } from "react-router-dom";
 import Home from "./Home"
 import Login from "./Login"
 import Register from "./Register"
 import Transaction from "./components/Transactions"
 
 function App() {
+    console.log(sessionStorage.getItem("token"));
+  if(sessionStorage.getItem("token") === null){
   return (
-    <BrowserRouter>
+    <>
+      <Redirect to="/login" />
+      <Login/>
+    </>
+  )
+  }
+  return (
         <div className={"h-full w-full"}>
-          <Redirect
-            from="/"
-            to="/home" />
           <Switch>
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
-            <Route path="/home" component={Home} />
+            <Route path={["/home", "/"]}>
+              <Redirect from="/" to="/home" />
+              <Home/>
+            </Route>
             <Route path="/transactions" component={Transaction} />
             <Route path="*"> </Route>
           </Switch>
         </div>
-    </BrowserRouter>
   )}
 export default App;
