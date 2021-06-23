dc473dbeb75051736c8352911ead8ffb7b73f8ec
singiamtel <sergiogarciacotos@gmail.com>
2021-06-23 21:54:06 +0200

    Indentation

--- src/models/portfolio.interface.ts
+++ src/models/portfolio.interface.ts
@@ -1,62 +1,64 @@
 export interface PortfolioResponseType
-	{
-		"status": string,
-		"wallet": {
-			"amount": number,
-			"targetPrice": number,
-			"api": {
-			  "meta": {
-				"symbol": string,
-				"currency": string,
-				"exchange": string,
-				"type": string
-			  },
-			  "values": [
-				{
-				  "datetime": string,
-				  "open": number,
-				  "close": number
-				},
-				{
-				  "datetime": string,
-				  "open":number,
-				  "close": number
-				}
-			  ],
-			  "status": string
-			}
-		}[]		
-	}
+{
+  "status": string,
+  "message": string,
+  errorCode?: number,
+  "wallet": {
+    "amount": number,
+    "targetPrice": number,
+    "api": {
+      "meta": {
+        "symbol": string,
+        "currency": string,
+        "exchange": string,
+        "type": string
+      },
+      "values": [
+        {
+          "datetime": string,
+          "open": number,
+          "close": number
+        },
+        {
+          "datetime": string,
+          "open":number,
+          "close": number
+        }
+      ],
+      "status": string
+    }
+  }[]
+}
 
 
-	export interface PortfolioAuxType
-	{
-		"wallet": PortfolioType[]		
-	}
+export interface PortfolioAuxType
+{
+  "wallet": PortfolioType[]		
+}
 
-	export interface PortfolioType
-	{
-		"amount": number,
-		"targetPrice": number,
-		"api": {
-		"meta": {
-			"symbol": string,
-			"currency": string,
-			"exchange": string,
-			"type": string
-		},
-		"values": [
-			{
-			"datetime": string,
-			"open": number,
-			"close": number
-			},
-			{
-			"datetime": string,
-			"open":number,
-			"close": number
-			}
-		],
-		"status": string
-		}
-	}[]		
+export interface PortfolioType
+{
+  "amount": number,
+  "targetPrice": number,
+  "api": {
+    "meta": {
+      "symbol": string,
+      "currency": string,
+      "exchange": string,
+      "type": string
+    },
+    "values": [
+      {
+        "datetime": string,
+        "open": number,
+        "close": number
+      },
+      {
+        "datetime": string,
+        "open":number,
+        "close": number
+      }
+    ],
+    "status": string
+  }
+}
